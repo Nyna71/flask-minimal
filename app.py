2acbdd0c71fc8534b0492f6aa6cddc062f9ba63f
Jonathan Puvilland <jonathan.puvilland@gmail.com>
2018-09-06 19:24:42 +0200

    Root Path

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/min')
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
